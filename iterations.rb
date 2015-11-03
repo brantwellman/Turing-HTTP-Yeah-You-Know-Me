9886be6a56b48aeff22a5284edf3389ad8d6cefd
Beth Secor <elizabeth.a.secor@gmail.com>
2015-11-03 13:52:58 -0700

    Added debug to iteration 2 and renamed to iterations

--- iterations.rb
+++ iterations.rb
@@ -14,19 +14,20 @@ loop do
   end
 
   path = hello_server.request_path(request_lines)
+  debug = hello_server.request_output(request_lines)
 
   if path == "/"
-    response = hello_server.request_output(request_lines)
+    response = debug
   elsif path == "/hello"
     unless request_lines.first == "GET /favicon.ico HTTP/1.1"
       hello_counter += 1
     end
-    response = "Hello World (#{hello_counter})"
+    response = "Hello World (#{hello_counter})" + "\n" + debug
   elsif path == "/datetime"
     now_time = DateTime.now
-    response = now_time.strftime('%H:%M%p on %A, %B %d, %Y')
+    response = now_time.strftime('%H:%M%p on %A, %B %d, %Y')  + "\n" + debug
   elsif path == "/shutdown"
-    response = "Total Requests #{request_counter}"
+    response = "Total Requests #{request_counter}"  + "\n" + debug
     hello_server.server_response(response)
     break
   end

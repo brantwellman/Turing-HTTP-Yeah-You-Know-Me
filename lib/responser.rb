1d89feac3b7b8a8897f8c25e7e7d6ae6ec0bf439
Brant Wellman <BrantWellman@gmail.com>
2015-11-04 14:27:45 -0700

    Responser Class fully tested. Not integrated

--- lib/responser.rb
+++ lib/responser.rb
@@ -1,5 +1,7 @@
 require_relative 'request_formatting'
 require_relative 'path_checker'
+require './lib/parameter_parser'
+require './lib/word_search'
 
 class Responser
   def initialize
@@ -7,7 +9,8 @@ class Responser
     # @response_strings = ResponseStrings.new
   end
 
-  def generate_response(path, request_lines, hello_counter, request_counter)
+  def generate_response(request_lines, hello_counter, request_counter)
+    path = @formatter.request_path(request_lines)
     path_checker = PathChecker.new(path)
     debug = generate_debug(request_lines)
 
@@ -19,6 +22,20 @@ class Responser
     elsif path_checker.datetime?
       now_time = DateTime.now
       response = now_time.strftime('%H:%M%p on %A, %B %d, %Y')  + "\n" + debug
+    elsif path_checker.word_search?
+      parser = ParameterParser.new
+      words = parser.parameters(path)
+      words_check = WordSearch.new
+      response = words_check.word_output(words)
+      response += debug
+    elsif path_checker.start_game?
+      if @formatter.verb(request_lines) == "POST"
+        response = "Good luck!" + debug
+      else
+        response = debug
+      end
+    elsif path_checker.shutdown?
+      response = "Total Requests #{request_counter}"  + "\n" + debug
     end
 
     response
--- test/responser_test.rb
+++ test/responser_test.rb
@@ -20,7 +20,7 @@ class ResponserTest < Minitest::Test
 
     expected = "<pre>Verb: GET\nPath: /\nProtocol: HTTP/1.1\nHost: 127.0.0.1\nPort: 9292\nOrigin: 127.0.0.1\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n</pre>"
 
-    assert_equal expected, responser.generate_response("/", request_lines, 0, 0)
+    assert_equal expected, responser.generate_response(request_lines, 0, 0)
   end
 
   def test_responds_with_hello_info_for_hello_path_on_request_zero
@@ -37,7 +37,7 @@ class ResponserTest < Minitest::Test
 
     expected = "Hello World (0)\n<pre>Verb: GET\nPath: /hello\nProtocol: HTTP/1.1\nHost: 127.0.0.1\nPort: 9292\nOrigin: 127.0.0.1\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n</pre>"
 
-    assert_equal expected, responser.generate_response("/hello", request_lines, 0, 0)
+    assert_equal expected, responser.generate_response(request_lines, 0, 0)
   end
 
   def test_responds_with_datetime_info_for_datetime_path
@@ -56,6 +56,59 @@ class ResponserTest < Minitest::Test
 
     expected = "#{date_time}\n<pre>Verb: GET\nPath: /datetime\nProtocol: HTTP/1.1\nHost: 127.0.0.1\nPort: 9292\nOrigin: 127.0.0.1\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n</pre>"
 
-    assert_equal expected, responser.generate_response("/datetime", request_lines, 0, 0)
+    assert_equal expected, responser.generate_response(request_lines, 0, 0)
+  end
+
+  def test_responds_with_word_validation_info_for_wordsearch_path
+    responser = Responser.new
+    request_lines = ["GET /word_search?word=pizza HTTP/1.1",
+                     "Host: 127.0.0.1:9292",
+                     "Connection: keep-alive",
+                     "Cache-Control: max-age=0",
+                     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                     "Upgrade-Insecure-Requests: 1",
+                     "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36",
+                     "Accept-Encoding: gzip, deflate, sdch",
+                     "Accept-Language: en-US,en;q=0.8"]
+
+
+    expected = "<pre>pizza is a known word.</pre><pre>Verb: GET\nPath: /word_search?word=pizza\nProtocol: HTTP/1.1\nHost: 127.0.0.1\nPort: 9292\nOrigin: 127.0.0.1\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n</pre>"
+
+    assert_equal expected, responser.generate_response(request_lines, 0, 0)
+  end
+
+  def test_responds_with_request_count_info_for_shutdown_path
+    responser = Responser.new
+    request_lines = ["GET /shutdown HTTP/1.1",
+                     "Host: 127.0.0.1:9292",
+                     "Connection: keep-alive",
+                     "Cache-Control: max-age=0",
+                     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                     "Upgrade-Insecure-Requests: 1",
+                     "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36",
+                     "Accept-Encoding: gzip, deflate, sdch",
+                     "Accept-Language: en-US,en;q=0.8"]
+
+    request_counter = 9
+    expected = "Total Requests #{request_counter}\n<pre>Verb: GET\nPath: /shutdown\nProtocol: HTTP/1.1\nHost: 127.0.0.1\nPort: 9292\nOrigin: 127.0.0.1\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n</pre>"
+
+    assert_equal expected, responser.generate_response(request_lines, 0, request_counter)
+  end
+
+  def test_responds_with_good_luck_for_post_start_game
+    responser = Responser.new
+    request_lines = ["POST /start_game HTTP/1.1",
+                     "Host: 127.0.0.1:9292",
+                     "Connection: keep-alive",
+                     "Cache-Control: max-age=0",
+                     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                     "Upgrade-Insecure-Requests: 1",
+                     "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36",
+                     "Accept-Encoding: gzip, deflate, sdch",
+                     "Accept-Language: en-US,en;q=0.8"]
+
+    expected = "Good luck!<pre>Verb: POST\nPath: /start_game\nProtocol: HTTP/1.1\nHost: 127.0.0.1\nPort: 9292\nOrigin: 127.0.0.1\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n</pre>"
+
+    assert_equal expected, responser.generate_response(request_lines, 0, 0)
   end
 end
